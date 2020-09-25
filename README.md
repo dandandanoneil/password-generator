46a054e04fda7ca778c3337a4daf442432417c84
Dan ONeil <dhoneil@gmail.com>
2020-09-25 13:27:39 -0400

    Added 'copy' and 'generate' buttons, added option to eliminate easily misread characters, removed given CSS and implemented Bootstrap and Bootwsatch styling

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Password Generator
 This web application automatically generates secure random passwords based on user inputs including length and whether or not to include lower case letters, upper case letters, numeric characters, and special characters.
 
+Other features include copying the generated password to the clipboard, generating a new password with the same criteria, and an option to eliminate easily misread characters (like 0, O, 1, I, and l).
+
 ## Screenshot
 [View the deployed web application.](https://dandandanoneil.github.io/password-generator/index.html)
 
-<img src="images/password-generator.png" alt="Password Generator main screen screenshot">
-<img src="images/password-generator-none-selected.png" alt="Password Generator user imput example screenshot">
+![Password Generator screenshot](images/password-generator.png)
 
 ## Next Steps
 I'm interested in developing this web application further in the following ways:
- - Including other options like eliminating easily misread or confused characters like 0, O, 1, I, l, etc.
- - Adding a dropdown list of recently geenrated passwords
+ - Adding a dropdown list that retrieves recently geenrated passwords
  - Updating or replacing the CSS to make the design/layout more my own
 
 > Written with [StackEdit](https://stackedit.io/).
--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const numericCheck = document.querySelector("#numeric");
 const specialCheck = document.querySelector("#special");
 const lengthRange = document.querySelector("#length");
 const rangeDisplay = document.querySelector("#rangeDisplay");
+const copyButton = document.querySelector("#copy");
+const generateButton = document.querySelector("#generate");
+const confusingCheck = document.querySelector("#confusing");
 
 // Create strings of character banks to draw from
 const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
@@ -19,9 +22,11 @@ upperCaseCheck.checked = true;
 numericCheck.checked = true;
 specialCheck.checked = true;
 lengthRange.value = 10;
+confusingCheck.checked - false;
 
 showLength();
 writePassword();
+console.log("This web application will log here each new password it generates. Often, the first password generated randomly will not meet all the user criteria, so the app will continue to generate new random options until a satisfactory password is generated.")
 
 
 // Generate a password based on user inputs & write it to the #password div
@@ -32,22 +37,24 @@ function writePassword() {
   let useUpperCase = upperCaseCheck.checked;
   let useNumeric = numericCheck.checked;
   let useSpecial = specialCheck.checked;
+  let makeReadable = confusingCheck.checked;
 
   // If no character types are selected, don't run generatPassword()
   if (!useLowerCase && !useUpperCase  && !useNumeric  && !useSpecial) {
-    passwordText.value = "You must select at least one character type!"
+    passwordText.value = "password123... just kidding. Select at least one character type!"
     return;
   }
 
   // Generate a new password with the prompted criteria. If the new value of password matches all the criteria, write it in the #password element. If it doesn't, keep generating new passowrds until one does.
   let isValid = false;
   while (!isValid) {
-    let password = generatePassword(passwordLength, useLowerCase, useUpperCase, useNumeric, useSpecial);
+    let password = generatePassword(passwordLength, useLowerCase, useUpperCase, useNumeric, useSpecial, makeReadable);
 
     if ((useLowerCase != commonChars(password, lowerCaseChars)) || 
     (useUpperCase != commonChars(password, upperCaseChars)) || 
     (useNumeric != commonChars(password, numericChars)) || 
-    (useSpecial != commonChars(password, specialChars)) ){
+    (useSpecial != commonChars(password, specialChars)) ||
+    (makeReadable == commonChars(password, "O0I1l")) ){
       isValid = false;
       console.log(password, isValid);
     } else {
@@ -61,13 +68,22 @@ function writePassword() {
 }
 
 // Generate a random password from the given criteria.
-function generatePassword(length, useLow, useUpp, useNum, useSpe) {
+function generatePassword(length, useLow, useUpp, useNum, useSpe, makeRea) {
   // Create a string of all possible characters to choose from
   let possibleChars = "";
   if(useLow) { possibleChars += lowerCaseChars; }
   if(useUpp) { possibleChars += upperCaseChars; }
   if(useNum) { possibleChars += numericChars; }
   if(useSpe) { possibleChars += specialChars; }
+
+  // If they want a more readble password, eliminate easily misread characters
+  if (makeRea) {
+    possibleChars = possibleChars.replace("O", "");
+    possibleChars = possibleChars.replace("0", "");
+    possibleChars = possibleChars.replace("I", "");
+    possibleChars = possibleChars.replace("l", "");
+    possibleChars = possibleChars.replace("1", "");
+  }
 
   let newPassword = "";
   for (let i = 0; i < length; i++) {
@@ -82,7 +98,15 @@ function generatePassword(length, useLow, useUpp, useNum, useSpe) {
 
 // Displays the value next to the range, then calls writePassword
 function showLength() {
-  rangeDisplay.textContent = lengthRange.value;
+  rangeDisplay.textContent = lengthRange.value + " characters";
+}
+
+// Copies the text from the read only password text area to the user's clipboard
+function copyPassword() {
+  // Select text
+  passwordText.select();
+  passwordText.setSelectionRange(0, 99999); /*For mobile devices*/
+  document.execCommand("copy");
 }
 
 // Checks whether two strings have any characters in common and returns a boolean
@@ -106,6 +130,11 @@ upperCaseCheck.addEventListener("click", writePassword);
 numericCheck.addEventListener("click", writePassword);
 specialCheck.addEventListener("click", writePassword);
 lengthRange.addEventListener("change", writePassword);
+confusingCheck.addEventListener("click", writePassword);
+
+// Event listeners for buttons
+copyButton.addEventListener("click", copyPassword);
+generateButton.addEventListener("click", writePassword);
 
 // Event listener to display range as the input is dragged
 lengthRange.addEventListener("input", showLength);
